dcf064295cd18bab0b5a97963ed55b7a34628966
agrecascino <chocola@animebitch.es>
2017-10-25 22:18:56 -0400

    Dag work.

--- src/dag.cpp
+++ src/dag.cpp
@@ -31,21 +31,79 @@ void CDAGSystem::PopulateSeedEpoch(uint64_t epoch) {
             //Repeatedly hashes (seed_epoch - start_epoch) times
             sph_blake256_context ctx;
             sph_blake256_init(&ctx);
-            sph_blake256(&ctx, seedCache[epoch], 32);
-            sph_blake256_close(&ctx,seedCache[epoch]);
+            sph_blake256(&ctx, seedCache[epoch].data(), 32);
+            sph_blake256_close(&ctx,seedCache[epoch].data());
         }
     }
 }
 
-void CDAGSystem::CreateGraphInPlace(uint64_t epoch, uint64_t size, std::array<uint8_t, 32> seed) {
-    cacheCache[epoch].resize(size, 0);
-    uint64_t items =
+uint32_t CDAGSystem::fnv(uint32_t v1, uint32_t v2) {
+    return ((v1 * FNV_PRIME) ^ v2) % UINT32_MAX;
+}
 
+bool CDAGSystem::is_prime(uint64_t num) {
+    if (num < 2)
+        return false;
+    if ((num % 2 == 0) && number > 2)
+        return false;
+
+    for(uint64_t i = 3; i < sqrt(num); i += 2) {
+        if(num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+uint64_t CDAGSystem::GetCacheSize(uint64_t epoch) {
+    uint64_t size = this->CACHE_BYTES_INIT + (this->CACHE_BYTES_GROWTH * round(sqrt(6*epoch)));
+    size -= HASH_BYTES;
+    while(!is_prime(size)) {
+        size -= MIX_BYTES;
+    }
+    return size;
+}
+
+uint64_t CDAGSystem::GetGraphSize(uint64_t epoch) {
+    return GetCacheSize(epoch) * this->CACHE_MULTIPLIER;
+}
+
+void CDAGSystem::CreateCacheInPlace(uint64_t epoch) {
+    uint64_t size = GetCacheSize(epoch);
+    this->cacheCache[epoch].resize(size, 0);
+    uint64_t items = size / this->HASH_BYTES;
+    sph_blake256_context ctx;
+    sph_blake256_init(&ctx);
+    sph_blake256(&ctx, this->seedCache[epoch].data(), this->HASH_BYTES);
+    sph_blake256_close(&ctx, this->cacheCache[epoch].data());
+    for(uint64_t i = 0; i < (items - 1); i++) {
+        sph_blake256_init(&ctx);
+        sph_blake256(&ctx, this->cacheCache[epoch].data() + i * this->HASH_BYTES, this->HASH_BYTES);
+        sph_blake256_close(&ctx, this->cacheCache[epoch].data() + (i+1)*this->HASH_BYTES);
+    }
+
+    for(uint32_t rounds = 0; rounds < this->CACHE_ROUNDS; rounds++) {
+        for(uint32_t i = 0; i < items; i++) {
+            uint8_t item[this->HASH_BYTES];
+            uint64_t current = ((i - 1 + items) % items);
+            uint64_t target = this->cacheCache[i*this->HASH_BYTES] % items;
+            for(uint64_t byte = 0; byte < this->HASH_BYTES; byte++) {
+                item[byte] = this->cacheCache[(current * this->HASH_BYTES) + byte] ^ this->cacheCache[(target * this->HASH_BYTES) + byte];
+            }
+            sph_blake256_init(&ctx);
+            sph_blake256(&ctx, item, 32);
+            sph_blake256_close(&ctx, this->cacheCache[i * this->HASH_BYTES]);
+        }
+    }
 }
 
 void CDAGSystem::PopulateCacheEpoch(uint64_t epoch) {
     if(!this->cacheCache[epoch].empty()) {
         return;
     }
-    PopulateSeedEpoch(epoch);
+    static CCriticalSection cs;
+    {
+        LOCK(cs);
+        PopulateSeedEpoch(epoch);
+        CreateCacheInPlace(epoch);
+    }
 }
--- src/dag.h
+++ src/dag.h
@@ -20,6 +20,35 @@ public:
 
 class CDAGSystem {
 private:
+    /** Constants */
+    const uint32_t DATASET_BYTES_INIT = 536870912;
+    const uint32_t DATASET_BYTES_GROWTH = 12582912;
+    const uint32_t CACHE_BYTES_INIT = 8388608;
+    const uint32_t CACHE_BYTES_GROWTH = 196608;
+    const uint32_t EPOCH_LENGTH = 400;
+    const uint32_t CACHE_MULTIPLIER = 64;
+    const uint32_t MIX_BYTES = 64;
+    const uint32_t HASH_BYTES = 32;
+    const uint32_t DATASET_PARENTS = 256;
+    const uint32_t CACHE_ROUNDS = 3;
+    const uint32_t ACCESSES = 64;
+    const uint32_t FNV_PRIME = 0x01000193;
+
+    /** Trivial primality check */
+    bool is_prime(uint64_t num);
+
+    /** FNV hash function */
+    uint32_t fnv(uint32_t v1, uint32_t v2);
+
+    /** Get cache size from epoch */
+    uint64_t GetCacheSize(uint64_t epoch);
+
+    /** Get graph size from epoch */
+    uint64_t GetGraphSize(uint64_t epoch);
+
+    /** Get graph node from cache */
+    CDAGNode DeriveGraphNode(uint64_t epoch, uint64_t i);
+
     /** Caches cache seeds in case of a need for regeneration */
     std::map<size_t, std::array<uint8_t, 32>> seedCache;
     /** Caches caches to verify blocks */
@@ -34,17 +63,19 @@ private:
     /** Populates graph of the epoch's epoch */
     void PopulateGraphEpoch(uint64_t epoch);
 
-    /** Actually creates a graph from seed and size data*/
-    void CreateGraphInPlace(uint64_t epoch, uint64_t size, std::array<uint8_t, 32> seed);
+    /** Actually creates a graph from seed and size data */
+    void CreateCacheInPlace(uint64_t epoch);
+
+    void CreateGraphInPlace(uint64_t epoch);
 
 public:
     /** Gets node from cache, much slower(on the order of 100x, but uses about that much less memory. */
-    CDAGNode GetNode(uint64_t i, int height);
+    CDAGNode GetNode(uint64_t i, int32_t height);
     /**
      * Gets node from cached graph, delayed to generate the needed graph(which may take several minutes), but once it
      * is generated, returning a node is effectively instant.
      */
-    CDAGNode GetNodeFromGraph(uint64_t i, int height);
+    CDAGNode GetNodeFromGraph(uint64_t i, int32_t height);
 
 };
 
